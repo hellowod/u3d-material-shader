2ffefb7162dce6b4be32b662a41d360c8102f5ef
abaojin <abaojin@gmail.com>
2018-03-09 11:41:29 +0800

    unity mono
    Signed-off-by: abaojin <abaojin@gmail.com>

---
+++ examples/Assets/Matrix/Mono.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mono : MonoBehaviour
+{
+    void Reset()
+    {
+        Debug.LogError("======Reset=====");
+    }
+
+    void Awake()
+    {
+        Debug.LogError("======Awake=====");
+    }
+
+    void OnEnable()
+    {
+        Debug.LogError("======OnEnable=====");
+    }
+
+    void Start()
+    {
+        Debug.LogError("======Start=====");
+    }
+
+    void FixedUpdate()
+    {
+        Debug.LogError("======FixedUpdate=====");
+    }
+
+    void Update()
+    {
+        Debug.LogError("======Update=====");
+    }
+
+    void LateUpdate()
+    {
+        Debug.LogError("======LateUpdate=====");
+    }
+
+    void OnWillRenderObject()
+    {
+        Debug.LogError("======OnWillRenderObject=====");
+    }
+
+    void OnGUI()
+    {
+        Debug.LogError("======OnGUI=====");
+    }
+
+    void OnDisable()
+    {
+        Debug.LogError("======OnDisable=====");
+    }
+
+    void OnDestroy()
+    {
+        Debug.LogError("======OnDestroy=====");
+    }
+}
